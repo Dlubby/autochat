08ce343e61c301b8bd69e7032c35bf4480c30c0b
Dank <76633816+Dlubby@users.noreply.github.com>
2021-04-04 12:46:49 -0400

    Create lol.lua

---
+++ lol.lua
@@ -0,0 +1,31 @@
+-- When u add to the list add one extra number to the x in math.random(1,x) and then type else if RandomText == (the new number)
+-- ONLY WOZ EDIT THIS LIST TILL I EXPLAIN IT
+
+local RandomText = math.random(1,11)
+if RandomText == 1 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Lemme Suh That  Dih RQ", 'All');
+elseif RandomText == 2 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("L", 'All');
+elseif RandomText == 3 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Card is my son.", 'All');
+elseif RandomText == 4 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Dank Is My Dad.", 'All');
+elseif RandomText == 5 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("straight-ware?? more like gay ware XD", 'All');
+elseif RandomText == 6 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("psy", 'All');
+elseif RandomText == 7 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Mic up kid, Danky 3000", 'All');
+elseif RandomText == 8 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("I'm Black.", 'All');
+elseif RandomText == 9 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Dude, My mom walked in on me 👊 🥩 and then started 👊 my 🥩", 'All');
+elseif RandomText == 10 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("We Are Farmers", 'All');
+elseif RandomText == 11 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Lol bro i bombed my school last night", 'All');
+elseif RandomText == 12 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("givingurmombackshots", 'All');
+elseif RandomText == 13 then
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Gay-Ware.", 'All');
+end
